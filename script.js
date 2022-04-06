cb0e234354ec0084e4107198c15089edb61e582a
Azeem <livelyazeem@gmail.com>
2022-04-06 17:14:55 +0500

    imported numeral to correct float calculations

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Cash Register
-
+let numeral = require('numeral');
 function checkCashRegister(price, cash, cid) {
     let changeDue = cash - price;
     let cashArr = [0.01, 0.05, 0.1, 0.25, 1, 5, 10, 20, 100];
@@ -55,3 +55,6 @@ function checkCashRegister(price, cash, cid) {
 
 console.log(JSON.stringify(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])));
 
+const money = numeral(0.1).add(0.2);
+
+console.log(money.value());
