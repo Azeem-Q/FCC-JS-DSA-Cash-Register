8206bdff6101e1a972ca497765c7433c3a9ca0e2
Azeem <livelyazeem@gmail.com>
2022-04-06 16:57:42 +0500

    still working

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Cash Register
+
 function checkCashRegister(price, cash, cid) {
     let changeDue = cash - price;
     let cashArr = [0.01, 0.05, 0.1, 0.25, 1, 5, 10, 20, 100];
@@ -52,5 +54,4 @@ function checkCashRegister(price, cash, cid) {
 }
 
 console.log(JSON.stringify(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])));
-
 
